ce2b7e630d32dd25ecf847f5f6bfe963245f0663
Jan Martin Vogt <jm.vogt@tum.de>
2022-06-14 19:33:39 +0200

    chore: update readme with pagemap

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,3 @@
-export type {PowerPlantType} from './PowerPlantCard/PowerPlantCard';
-export {EnergyType} from './PowerPlantCard/PowerPlantCard';
 export {PowerPlantCard} from './PowerPlantCard/PowerPlantCard';
 export {StatusDisplay} from './StatusDisplay/StatusDisplay';
 export {Header} from './Header/Header';
