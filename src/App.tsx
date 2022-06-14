b974e0919bd71ac9a373e8860cd22821a8184892
aybarsyazici <aybarsyazici@sabanciuniv.edu>
2022-06-14 15:56:23 +0200

    feat: added login feature and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import { Layout } from './components/Layout/Layout';
-import Dashboard from "./pages/Dashboard"
-import Login from "./pages/Login"
-import UserSettings from "./pages/UserSettings";
+import { Layout } from './components';
+import { Dashboard, UserSettings, Login } from './pages';
+import { createGlobalState } from 'react-hooks-global-state';
+
+const initialState = { count: 0 };
+const { useGlobalState } = createGlobalState(initialState);
 
 export function App() {
     return (
         <Routes>
             <Route element={<Layout />}>
                 <Route path="/" element={<Dashboard />} />
-                <Route path="login" element={<Login />} />
                 <Route path="user-settings" element={<UserSettings />} />
             </Route>
+            <Route path="login" element={<Login />} />
         </Routes>
     )
 }
---
+++ src/state/index.ts
@@ -0,0 +1,41 @@
+import {createStore} from "react-hooks-global-state";
+
+type State = {
+  loginType?: 'Consumer' | 'Supplier';
+  loggedIn: boolean;
+}
+
+type Action = { type: 'setLoginType'; loginType: 'Consumer' | 'Supplier'}
+ | { type: 'logout' };
+
+const defaultState: State = {
+  loginType: undefined,
+  loggedIn: false,
+};
+
+const LOCAL_STORAGE_KEY = 'my_local_storage_key';
+
+const stateFromStorage = (localStorage.getItem(LOCAL_STORAGE_KEY));
+const initialState: State = stateFromStorage === null ? defaultState : JSON.parse(stateFromStorage);
+
+const reducer = (state:State, action:Action) => {
+  switch (action.type) {
+    case 'setLoginType':{
+      const newState = { ...state, LoginType: action.loginType, loggedIn: true };
+      console.log(newState);
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newState));
+      return newState;
+    }
+    case 'logout':{
+      const newState = {...state, LoginType: undefined, loggedIn: false};
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newState));
+      return newState;
+    }
+    default: return state;
+  }
+};
+
+export const { dispatch, useStoreState } = createStore(
+  reducer,
+  initialState,
+);
