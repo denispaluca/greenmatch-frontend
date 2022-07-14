6e2298d936bdb458ce9874718e655ec01af2cee5
Sebastian Schramm <s.schramm@tum.de>
2022-07-14 17:22:34 +0200

    feat: add energy type icons

--- src/components/PowerPlantCard/PowerPlantCard.tsx
+++ src/components/PowerPlantCard/PowerPlantCard.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react/jsx-key */
 import { Card, Space } from 'antd';
-import { SettingOutlined } from '@ant-design/icons';
+import Icon, { SettingOutlined } from '@ant-design/icons';
 import { Link, useNavigate } from 'react-router-dom';
 import { StatusDisplay } from '../StatusDisplay/StatusDisplay';
 import { PowerPlant } from '../../types/powerplant';
+import drop from '../../assets/images/icons/drop.png';
+import windmill from '../../assets/images/icons/windmill.png';
+import sun from '../../assets/images/icons/sun.png';
 
 
 interface PowerPlantProps {
@@ -14,7 +18,7 @@ export function PowerPlantCard({ powerPlant }: PowerPlantProps) {
   const navigate = useNavigate();
   return (
     <Card
-      style={{ borderRadius: '20px', overflow: 'hidden' }}
+      style={{ borderRadius: '10px', overflow: 'hidden' }}
       hoverable
       onClick={(event) => {
         navigate('/ppa-overview/' + powerPlant._id);
@@ -33,7 +37,19 @@ export function PowerPlantCard({ powerPlant }: PowerPlantProps) {
       ]}
     >
       <p>Location: {powerPlant.location}</p>
-      <p>Type: {powerPlant.energyType}</p>
+      <Space>
+        <span>Type:</span>
+        <span />
+        <span />
+        <Icon
+          component={() => (<img
+            width={'20px'}
+            src={sun}
+            alt="drop"
+          />)}
+        />
+        <span>{powerPlant.energyType}</span>
+      </Space>
     </Card>
   );
 }
