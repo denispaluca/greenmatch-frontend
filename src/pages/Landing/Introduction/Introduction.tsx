66bc18fdaddb37e3021a3285c6621805798e6586
Sebastian Schramm <s.schramm@tum.de>
2022-07-20 14:14:24 +0200

    fix: css adjustments

--- src/pages/Landing/Introduction/Introduction.tsx
+++ src/pages/Landing/Introduction/Introduction.tsx
@@ -3,11 +3,10 @@
 /* eslint-disable max-len */
 import { Button, Space } from 'antd';
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './Introduction.module.scss';
 
 const Introduction: React.FC = () => {
-  const navigate = useNavigate();
   return (
     <div
       className={styles.introduction}
@@ -16,18 +15,19 @@ const Introduction: React.FC = () => {
       <div className={styles.content}>
         <p>Hi, we are GreenMatch 👋</p>
         <p>A B2B marketplace for green, renewable energy PPAs</p>
-        <Space>
+        <Space >
           <Button
             style={{ width: 300, height: 70, fontSize: '2rem' }}
             ghost
           >
             <Link to="/register">Register Now</Link>
           </Button>
-          <span
-            onClick={() => navigate('/offers')}
+          <a
+            className={styles.offers}
+            href='/offers'
           >
-            ... or just explore the offerings
-          </span>
+            or just explore the offerings
+          </a>
         </Space>
       </div>
     </div >
